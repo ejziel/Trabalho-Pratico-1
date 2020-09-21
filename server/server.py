dcd7a22980b191491536e1b3b7dabaa7068b9327
Ejziel Santos <ziel_ss@hotmail.com>
2020-09-21 19:35:44 -0300

    finalizado

--- server/server.py
+++ server/server.py
@@ -7,37 +7,9 @@ import tqdm
 import os
 from io import BytesIO
 import threading
-
+import logging
 MAX_SIZE = 64*1048576
 
-class RWLock:
-    #construtor
-    def __init__(self):
-        self.readers = 0
-        self.mutex = threading.Semaphore(1) #Semáforo de leitura
-        self.lock = threading.Semaphore(1)  #Semáforo de escrita
- 
-    def read_acquire(self):
-        self.mutex.acquire() #bloqueia para leitura
-        self.readers += 1 #soma a quantidade de leitores
-        if self.readers == 1:
-            self.lock.acquire() #Se você é o primeiro leitor, bloqueie o recurso dos escritores.
-                                # Recurso permanece reservado para leitores subseqüentes
-        self.mutex.release() # desbloqueia
- 
-    def read_release(self):
-        self.mutex.acquire() #bloqueia para leitura
-        self.readers -= 1
-        if self.readers == 0:
-            self.lock.release() #Se você for o último leitor, poderá desbloquear o recurso. 
-                                # Isso torna disponível para escritores.
-        self.mutex.release() #desbloqueia
- 
-    def write_acquire(self):
-        self.lock.acquire() #bloqueia o semaforo de escrita
- 
-    def write_release(self):
-        self.lock.release() #desbloqueia
 
 class LRUCache:
 
@@ -63,7 +35,6 @@ class LRUCache:
 
     # put size and value of the file in the cache
     def put(self, key, size, value) -> None:
-        print (self.used_capacity)
         if(size > self.max_capacity):
             return -1
         while key not in self.cache:
@@ -83,24 +54,28 @@ class LRUCache:
         return key_list
 
 def send_file(client_socket, address, requested_filename):
-    # print client ip and file who is requested
+    # displays client address and requested file
     print(f"[+] Client {address} is requesting file {requested_filename}.\n")
-    lock.read_acquire()
 
-    time.sleep(10)
     if(cache.isIn(requested_filename)):
         print(f"[+] Cache hit. File {requested_filename} sent to the client.")
+
+        # locking
+        lock.acquire()
 
         # get value for the requested file
         value = cache.get(requested_filename)
 
-        # the filesize is the first element of the tuple
+        # release
+        lock.release()
+
+        # get filesize
         filesize = value[0]
 
         # send the bool and filesize
         client_socket.send(f"{1}{SEPARATOR}{filesize}".encode())
 
-        # the bytes of the archive is the second element
+        # load file from cache
         bin_file = pickle.loads(value[1])
 
         # start sending the file
@@ -123,8 +98,11 @@ def send_file(client_socket, address, requested_filename):
             print(f"[+] Cache missed. File {requested_filename} sent to the client.")
             
             if(filesize <= MAX_SIZE):
+                
+                # locking
+                lock.acquire()
 
-                #putting file in the cache
+                # putting file in the cache
                 with open(direc+"/"+requested_filename, "rb") as arq:
                     data = arq.read()
                     data = pickle.dumps(data)
@@ -136,6 +114,9 @@ def send_file(client_socket, address, requested_filename):
                 # get value for the requested file
                 value = cache.get(requested_filename)
                 
+                # release
+                lock.release()
+
                 # the bytes of the archive is the second element
                 bin_file = pickle.loads(value[1])
         
@@ -178,19 +159,19 @@ def send_file(client_socket, address, requested_filename):
 
     # close the client socket
     client_socket.close()
-    lock.read_release()
+    
 
 def send_list(client_socket, address):
     print(f"[+] Client {address} is requesting the cached files.\n")
-    lock.read_acquire()
+    #locking
+    lock.acquire()
 
-    #print(threading.current_thread().ident, "Reading:")
-    time.sleep(10)
     data = pickle.dumps(cache.cacheList())
     client_socket.sendall(data)
-    #time.sleep(0.5)
     client_socket.close()
-    lock.read_release()
+
+    #release
+    lock.release()
     
 
 
@@ -202,7 +183,6 @@ direc = sys.argv[2]
 host = "localhost"
 
 # receive 4096 bytes each time
-#BUFFER_SIZE = 4096
 BUFFER_SIZE = 1024
 SEPARATOR = "<SEPARATOR>"
 
@@ -219,29 +199,24 @@ print(f"[*] Listening as {host}:{port}")
 cache = LRUCache(MAX_SIZE) #VERIFICAR CACHE
 
 # create locker
-lock = RWLock()
+lock = threading.Lock()
 
 # main loop
 while True:
 
-    print(f"[*] Waiting connection.\n")
+    #print(f"[*] Waiting connection.\n")
     client_socket, address = s.accept()
 
     requested_item = client_socket.recv(BUFFER_SIZE).decode()
 
     if (requested_item == "cache_list"):  
-        #print("Main    : creating thread")
+        # creating thread
         x = threading.Thread(target=send_list, args=(client_socket, address))
         x.start()
-        #print("Main    : running thread")
-        #send_list(client_socket, address)
     else:
-        #print("Main    : creating thread")
+        # creating thread
         x = threading.Thread(target=send_file, args=(client_socket, address, requested_item))
         x.start()
-        #print("Main    : running thread")
-        #send_file(client_socket, address, requested_item) #VERIFICAR CACHE
-
 
 # close the server socket
 s.close()
